8efd4bb046ff87bd1f1d9ef88c4c0a6a2cec34e7
Imanam <manami.christophe@gmail.com>
2019-10-22 13:38:15 +0200

    Duplicate project with Flask

---
+++ app/routes.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+
+
+from flask import render_template
+
+
+from app import app
+from app.templates.database import get_users
+
+
+@app.route("/blah")
+def route_blah():
+    return "Salut tout le monde !"
+
+#@lru_cache
+
+
+@app.route("/")
+@app.route("/index")
+def route_index():
+    date = datetime.now()
+    users = get_users()
+    return render_template(
+        "index.html",
+        date_to_print=date,
+        users=users
+    )
+
+'''
+@app.route("user/<id>")
+def route_user(id):
+    table = [i for i in range(10) if i % 3 != 0]
+    ids = [u["id"] for u in get_users()]
+    user = [user for user in get_users() if user["id"] == id]
+    return "Utilisateur d'ID {} s'appel {}".format(id, user[0]["name"])
+'''
+
+@app.route("/user/<int:id>")
+def route_user(id):
+    user = [user for user in get_users() if user["id"] == id]
+    if len(user) > 0:
+        return f"Utilisateur d'ID {id} s'appel { user[0]['name']}"
+    else:
+        return "L'utilisateur n'existe pas!"
---
+++ app/templates/database.py
@@ -0,0 +1,12 @@
+def get_users():
+    return [
+        {"name": "user1", "id": 1, "sex": "Male"},
+        {"name": "user2", "id": 2, "sex": "Female"},
+        {"name": "user3", "id": 3, "sex": "Male"},
+        {"name": "user4", "id": 4, "sex": "Non-binaire"},
+        {"name": "user5", "id": 5, "sex": "Female"},
+        {"name": "user6", "id": 6, "sex": "Male"},
+        {"name": "user7", "id": 7, "sex":  None},
+        {"name": "user8", "id": 8, "sex": "Male"},
+
+    ]
